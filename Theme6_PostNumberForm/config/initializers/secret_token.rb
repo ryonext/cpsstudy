d00dedfa636376a37efe14d1139bbf4c5701b637
ryonext <ryo.next@gmail.com>
2012-07-25 23:16:16 +0900

    add theme6pre

---
+++ Theme6_PostNumberForm/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Theme6Postnumberform::Application.config.secret_token = 'ef90ea88ff6af23d6219755ddc3f874ca08bf457fcf43d53836a80eb2d4583efc85c5ecdecb6ca7eafd0d69ccc0d4ab71b5a4206b499d98ed16088690328cb48'
